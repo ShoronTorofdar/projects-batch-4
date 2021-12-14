84f339e86834e3e33c6eefb81d44ee53f82df696
ShoronTorofdar <86644344+ShoronTorofdar@users.noreply.github.com>
2021-12-15 01:16:37 +0600

    48-4-5.31

---
+++ ema-john-simple/src/components/Product/Product.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+  console.log(props.product)
+  return (
+    <div>
+      <h2>This is Product</h2>
+    </div>
+  );
+};
+
+export default Product;
